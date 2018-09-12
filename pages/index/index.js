9bc3e0436b00b7930c2316a1a77ada0efea88260
shenyoujian <shenyoujian2.163.com>
2018-09-12 20:11:11 +0800

    完成首页基本显示

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,62 @@
-//index.js
-//获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    // 用于分页的属性
+    totalPage: 1,
+    page: 1,
+    videoList: [],
+
+
+    screenWidth: 350,
+    serverUrl: ""
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+
+  onLoad: function(params) {
+    var me = this;
+    //调用微信接口获取手机屏幕的宽度
+    var screenWidth = wx.getSystemInfoSync().screenWidth;
+    me.setData({
+      screenWidth: screenWidth,
+    });
+
+    // 获取当前的分页数
+    var page = me.data.page;
+    var serverUrl = app.serverUrl;
+
+    wx.showLoading({
+      title: '请等待，加载中...',
     })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+
+    //开始请求后端
+    wx.request({
+      url: serverUrl + '/video/showAll?page=' + page,
+      method: 'POST',
+      success: function(res) {
+        wx.hideLoading();
+
+        console.log(res.data);
+
+        // 判断当前是否是第一页，如果是第一页那么videoList为空
+        if (page == 1) {
+          me.setData({
+            videoList: []
+          });
         }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+
+        //从后台请求到的视频列表
+        var videoList = res.data.data.rows;
+        //本来的视频列表
+        var newvideoList = me.data.videoList;
+
+        me.setData({
+          videoList: newvideoList.concat(videoList),
+          page: page,
+          totalPage: res.data.data.total,
+          serverUrl: serverUrl
+        });
+      }
     })
   }
+
 })
